c27f6794d585068821928a287b87e5ce8b58538f
Nisa Champagne <nchampag23@gmail.com>
2020-02-24 15:54:46 -0500

    steps 1 and 2 done, class notes added, working on step 3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,62 @@
 
 import sys
 
+'''
+MAY NEED VARIABLES FOR THESE LATER
+* `LDI`: load "immediate", store a value in a register, or "set this register to
+  this value".
+* `PRN`: a pseudo-instruction that prints the numeric value stored in a
+  register.
+* `HLT`: halt the CPU and exit the emulator.
+'''
+
+
 class CPU:
     """Main CPU class."""
 
+    '''
+    Add list properties to the `CPU` class to hold 256 bytes of memory and 8
+    general-purpose registers.
+
+    > Hint: you can make a list of a certain number of zeros with this syntax:
+    >
+    > ```python
+    > x = [0] * 25  # x is a list of 25 zeroes
+    > ```
+
+    Also add properties for any internal registers you need, e.g. `PC`.
+
+    Later on, you might do further initialization here, e.g. setting the initial
+    value of the stack pointer.
+    '''
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
+    '''
+    In `CPU`, add method `ram_read()` and `ram_write()` that access the RAM inside
+    the `CPU` object.
+
+    `ram_read()` should accept the address to read and return the value stored
+    there.
+
+    `raw_write()` should accept a value to write, and the address to write it to.
+
+    > Inside the CPU, there are two internal registers used for memory operations:
+    > the _Memory Address Register_ (MAR) and the _Memory Data Register_ (MDR). The
+    > MAR contains the address that is being read or written to. The MDR contains
+    > the data that was read or the data to write. You don't need to add the MAR or
+    > MDR to your `CPU` class, but they would make handy paramter names for
+    > `ram_read()` and `ram_write()`, if you wanted.
+    '''
+
+    def ram_read(self,mem_address):
+        return self.ram[mem_address]
+
+    def ram_write(self, value, mem_address):
+        self.ram[mem_address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +112,24 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+        '''
+        It needs to read the memory address that's stored in register `PC`, and store
+        that result in `IR`, the _Instruction Register_. This can just be a local
+        variable in `run()`.
+
+        Some instructions requires up to the next two bytes of data _after_ the `PC` in
+        memory to perform operations on. Sometimes the byte value is a register number,
+        other times it's a constant value (in the case of `LDI`). Using `ram_read()`,
+        read the bytes at `PC+1` and `PC+2` from RAM into variables `operand_a` and
+        `operand_b` in case the instruction needs them.
+
+        Then, depending on the value of the opcode, perform the actions needed for the
+        instruction per the LS-8 spec. Maybe an `if-elif` cascade...? There are other
+        options, too.
+
+        After running code for any particular instruction, the `PC` needs to be updated
+        to point to the next instruction for the next iteration of the loop in `run()`.
+        The number of bytes an instruction uses can be determined from the two high bits
+        (bits 6-7) of the instruction opcode. See the LS-8 spec for details.
+        '''
         pass
